a32ac85a74f3f8ccd77c3248e663baabd4a6bd4b
Gowtham_S <145690539+Gowthamkar123@users.noreply.github.com>
2025-03-14 14:43:13 +0530

    newjexps.js

--- newjexps.js
+++ newjexps.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+
 
 // In-memory storage for posts
 let posts = [];
